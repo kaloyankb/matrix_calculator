34fb257f9b0087edf5e05eb617786d457954f632
kaloyankb <kalo.fire@abv.bg>
2021-01-03 20:28:03 +0200

    project started and added print matrix function

---
+++ project_mat_calc.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+const int MAX_SIZE = 10000;
+int m, n;
+
+
+void printMat(int m, int n, double ** mat)
+{
+
+    for(int i=0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            cout << mat[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main ()
+{
+
+    retrun 0;
+}
