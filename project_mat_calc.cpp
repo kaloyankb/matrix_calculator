370253f3d8306d3c1e1d24def8e96abb30505fdd
kaloyankb <kalo.fire@abv.bg>
2021-01-04 23:37:18 +0200

    added initialize, sum, scalar multiply and scalar dicide dunctions

--- project_mat_calc.cpp
+++ project_mat_calc.cpp
@@ -4,6 +4,15 @@ using namespace std;
 const int MAX_SIZE = 10000;
 int m, n;
 
+void initializeMat(int m, int n, double ** mat){
+for(int i = 0; i < m; i++){
+        for(int j = 0; j < n; j++){
+            cin >> mat [i][j];
+        }
+    }
+
+}
+
 
 void printMat(int m, int n, double ** mat)
 {
@@ -18,8 +27,51 @@ void printMat(int m, int n, double ** mat)
     }
 }
 
+void sumMatrices(int m, int n, double ** mat1, double ** mat2){
+    double** sum = new double* [m];
+    for(int i = 0; i < m; i++){
+        sum[i] = new double[n];
+    }
+
+    for(int i=0; i < m; i++){
+        for(int j = 0; j < n; j++){
+            sum[i][j]= mat1[i][j] + mat2[i][j];
+        }
+    }
+    printMat(m, n, sum);
+
+}
+
+void scalMultiply(int m, int n, double scal, double ** mat){
+    double ** multiplyScal = new double * [m];
+    for(int i = 0; i < m; i++){
+        multiplyScal[i] = new double[n];
+    }
+
+    for(int i=0; i < m; i++){
+        for(int j = 0; j < n; j++){
+            multiplyScal[i][j]= mat[i][j] * scal;
+        }
+    }
+    printMat(m, n, multiplyScal);
+}
+
+void scalDivide(int m, int n, double scal, double ** mat){
+    double ** divideScal = new double * [m];
+    for(int i = 0; i < m; i++){
+        divideScal[i] = new double[n];
+    }
+
+    for(int i=0; i < m; i++){
+        for(int j = 0; j < n; j++){
+            divideScal[i][j]= mat[i][j] / scal;
+        }
+    }
+    printMat(m, n, divideScal);
+}
+
 int main ()
 {
 
-    retrun 0;
+    return 0;
 }
