3be47a0ec68ee825ac8ac093992fed6ca43beee3
kaloyankb <kalo.fire@abv.bg>
2021-01-07 23:43:59 +0200

    added file read function, updated main function and added text document to the submition

--- project_mat_calc.cpp
+++ project_mat_calc.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 
-void initializeMat(int m, int n, double ** mat){
-for(int i = 0; i < m; i++){
-        for(int j = 0; j < n; j++){
+void initializeMat(int m, int n, double ** mat)
+{
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
             cin >> mat [i][j];
         }
     }
@@ -24,43 +28,37 @@ void printMat(int m, int n, double ** mat)
     }
 }
 
-void sumMatrices(int m, int n, double ** mat1, double ** mat2){
-    double** sum = new double* [m];
-    for(int i = 0; i < m; i++){
-        sum[i] = new double[n];
-    }
 
-    for(int i=0; i < m; i++){
-        for(int j = 0; j < n; j++){
-            sum[i][j]= mat1[i][j] + mat2[i][j];
-        }
-    }
-    printMat(m, n, sum);
-
-}
-
-void scalMultiply(int m, int n, double scal, double ** mat){
+void scalMultiply(int m, int n, double scal, double ** mat)
+{
     double ** multiplyScal = new double * [m];
-    for(int i = 0; i < m; i++){
+    for(int i = 0; i < m; i++)
+    {
         multiplyScal[i] = new double[n];
     }
 
-    for(int i=0; i < m; i++){
-        for(int j = 0; j < n; j++){
+    for(int i=0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
             multiplyScal[i][j]= mat[i][j] * scal;
         }
     }
     printMat(m, n, multiplyScal);
 }
 
-void scalDivide(int m, int n, double scal, double ** mat){
+void scalDivide(int m, int n, double scal, double ** mat)
+{
     double ** divideScal = new double * [m];
-    for(int i = 0; i < m; i++){
+    for(int i = 0; i < m; i++)
+    {
         divideScal[i] = new double[n];
     }
 
-    for(int i=0; i < m; i++){
-        for(int j = 0; j < n; j++){
+    for(int i=0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
             divideScal[i][j]= mat[i][j] / scal;
         }
     }
@@ -92,79 +90,97 @@ void poddeterminantaUse (int m, int n, double ** mat, int removedElementI, int r
     }
 }
 
-void transp(int m, int n, double ** mat){
+void transp(int m, int n, double ** mat)
+{
     double ** matTr = new double * [m];
     for(int i = 0; i < m; i++)
     {
         matTr[i] = new double [n];
     }
-    for(int i = 0; i < m; i++) {
-    for (int j = 0; j < n; j++){
-        matTr[i][j] = mat[j][i];
+    for(int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matTr[i][j] = mat[j][i];
         }
     }
     printMat(m, n, matTr);
 }
 
-int determinant (int m, int n, double ** mat, double det = 0){
+int determinant (int m, int n, double ** mat, double det = 0)
+{
     double detForReturn = 0;
     double ** mCopy = new double * [m];
     for(int i = 0; i < m; i++)
     {
         mCopy[i] = new double [n];
     }
-    for(int i = 0; i < m; i++) {
-    for (int j = 0; j < n; j++){
-        mCopy[i][j] = mat[i][j];
+    for(int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            mCopy[i][j] = mat[i][j];
         }
     }
-    if(m == n){
-        if(m == 1){
+    if(m == n)
+    {
+        if(m == 1)
+        {
             det = mat[0][0];
             return det;
         }
-        if(m == 2){
+        if(m == 2)
+        {
             det = mat[0][0] * mat[1][1] - mat[0][1] * mat[1][0];
             return det;
         }
-        if(m == 3){
+        if(m == 3)
+        {
             det = mat[0][0] * mat[1][1] * mat[2][2] + mat[0][1] * mat[1][2] * mat[2][0] + mat[0][2] * mat[1][0] * mat[2][1];
             det -= (mat[0][2] * mat[1][1] * mat[2][0] + mat[0][0] * mat[1][2] * mat[2][1] + mat[0][1] * mat[1][0] * mat[2][2]);
             return det;
         }
-        if(m == 4){
-                for(int j = 0; j < n; j++){
-                      //  cout << "j: " << j << endl;
-                    if(j % 2 == 0){
-                        int elRem = mat[0][j];
-                        poddeterminantaUse(m, n, mCopy, 0, j);
-                       // printMat(m - 1, n - 1, mCopy);
-                        //cout << endl;
-                        //cout << "here" << endl;
-                        det += elRem * determinant(m - 1, n - 1, mCopy);
+        if(m == 4)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                //  cout << "j: " << j << endl;
+                if(j % 2 == 0)
+                {
+                    int elRem = mat[0][j];
+                    poddeterminantaUse(m, n, mCopy, 0, j);
+                    // printMat(m - 1, n - 1, mCopy);
+                    //cout << endl;
+                    //cout << "here" << endl;
+                    det += elRem * determinant(m - 1, n - 1, mCopy);
 
-                        for(int i = 0; i < m; i++) {
-                            for (int j = 0; j < n; j++){
-                                mCopy[i][j] = mat[i][j];
-                            }
+                    for(int i = 0; i < m; i++)
+                    {
+                        for (int j = 0; j < n; j++)
+                        {
+                            mCopy[i][j] = mat[i][j];
                         }
-
-                       // cout << "yaaa, the det is: " << det;
                     }
-                    else {
-                        int elRem = mat [0][j];
-                        poddeterminantaUse(m, n, mCopy, 0, j);
-                        det += -(elRem * determinant(m - 1, n - 1, mCopy));
 
-                        for(int i = 0; i < m; i++) {
-                            for (int j = 0; j < n; j++){
-                                mCopy[i][j] = mat[i][j];
-                            }
-                        }
-                        detForReturn = det;
-                    }
+                    // cout << "yaaa, the det is: " << det;
                 }
-                return detForReturn;
+                else
+                {
+                    int elRem = mat [0][j];
+                    poddeterminantaUse(m, n, mCopy, 0, j);
+                    det += -(elRem * determinant(m - 1, n - 1, mCopy));
+
+                    for(int i = 0; i < m; i++)
+                    {
+                        for (int j = 0; j < n; j++)
+                        {
+                            mCopy[i][j] = mat[i][j];
+                        }
+                    }
+                    detForReturn = det;
+                }
+            }
+            return detForReturn;
         }
     }
     else cout << "the dimensions of the matrix should be equal" << endl;
@@ -187,14 +203,17 @@ void multiplyMats(double ** mat1, double ** mat2, int m1, int n1, int m2, int n2
     else
     {
 
-        for(int i = 0; i < m1; i++){
-            for(int j = 0; j < n2; j++){
-                for(int k = 0; k < m2; k++){
-                   multiply[i][j] += mat1[i][k] * mat2[k][j];
+        for(int i = 0; i < m1; i++)
+        {
+            for(int j = 0; j < n2; j++)
+            {
+                for(int k = 0; k < m2; k++)
+                {
+                    multiply[i][j] += mat1[i][k] * mat2[k][j];
                 }
             }
         }
-    printMat(m1, n2, multiply);
+        printMat(m1, n2, multiply);
     }
 }
 
@@ -294,25 +313,52 @@ char endMesseges()
 }
 
 
+int loadFile(char fileName[])
+{
+    ifstream file(fileName);
+    if (!file.is_open())
+    {
+        return -1;
+    }
+    const int MAX_SIZE = 150;
+    char str[MAX_SIZE];
+
+    while (!file.eof())
+    {
+        file.getline(str, MAX_SIZE);
+
+        cout << str << endl;
+    }
+    file.close();
+    return 0;
+
+}
 
 
 int main ()
 {
+    cout << "Enter file name:" <<endl;
+    char myStr[200] = {};
+    cin.getline(myStr, 200);
+    cout << endl;
+    loadFile(myStr);
+
     while(true)
     {
         cout << "Type the number of the operation you want to use: " << endl;
-
         char choice [1] = {};
         cin >> choice[0];
         int m1, n1, m2, n2 = 0;
         cout << "Set the two dimensions of the matrix" << endl;
         if(choice[0] == '1') cout << "(First matrix)" << endl;
         cin >> m1 >> n1;
-        if(choice[0] == '5' || choice[0] == '6'){
-            while(m1 != n1){
-            cout << "The dimensions of the matrix should be equal" << endl;
-            cout << "Set the two dimensions of the matrix" << endl;
-            cin >> m1 >> n1;
+        if(choice[0] == '5' || choice[0] == '6')
+        {
+            while(m1 != n1)
+            {
+                cout << "The dimensions of the matrix should be equal" << endl;
+                cout << "Set the two dimensions of the matrix" << endl;
+                cin >> m1 >> n1;
             }
         }
         double** myMat1 = new double* [m1];
@@ -323,21 +369,23 @@ int main ()
         cout << "Now initialize the matrix" << endl;
         initializeMat(m1, n1, myMat1);
 
-        if(choice[0] == '1'){
+        if(choice[0] == '1')
+        {
             cout << "Set the two dimensions for the matrix" << endl;
             cout << "(Second one)" << endl;
             cin >> m2 >> n2;
-            while(m2 != n1){
-                 cout << "the second dimension of the first matrix and the first dimension of the second matrix should be equal" << endl;
-                 cout << "Set the two dimensions for the matrix" << endl;
-                 cout << "(Second matrix)" << endl;
-                 cin >> m2 >> n2;
+            while(m2 != n1)
+            {
+                cout << "the second dimension of the first matrix and the first dimension of the second matrix should be equal" << endl;
+                cout << "Set the two dimensions for the matrix" << endl;
+                cout << "(Second matrix)" << endl;
+                cin >> m2 >> n2;
             }
             cout << "Now initialize the matrix" << endl;
             double** myMat2 = new double* [m2];
             for(int i = 0; i < m2; i++)
             {
-            myMat2[i] = new double[n2];
+                myMat2[i] = new double[n2];
             }
             initializeMat(m2, n2, myMat2);
             cout << "The answer is: " << endl;
@@ -373,7 +421,8 @@ int main ()
                 break;
             }
         }
-        if(choice[0] == '4'){
+        if(choice[0] == '4')
+        {
             cout << "The transposed matrix is: " << endl;
             transp(m1, n1, myMat1);
             if(endMesseges() == 'F')
@@ -381,22 +430,28 @@ int main ()
                 break;
             }
         }
-        if(choice[0] == '5'){
+        if(choice[0] == '5')
+        {
             cout << "The determinant of the matrix is: " << endl;
             cout << determinant(m1, n1, myMat1) << endl;
-             if(endMesseges() == 'F')
+            if(endMesseges() == 'F')
             {
                 break;
             }
         }
-        if(choice[0] == '6'){
+        if(choice[0] == '6')
+        {
             opositeMat(m1, n1, myMat1);
             if(endMesseges() == 'F')
             {
                 break;
             }
         }
+        cout << endl << endl;
+        loadFile(myStr);
     }
+
+    cout << endl <<"Thank you for using the matrix calculator! Goodbye!" << endl;
 
     return 0;
 }
